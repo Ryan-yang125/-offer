4b3e043f4e07e9e3f43d0a32e6d620cc663a55fa
Ryan-yang125 <1598094295@qq.com>
2020-10-18 10:51:40 +0800

    jusity file structure

---
+++ 链表/JZ36两个链表的第一个公共结点.cc
@@ -0,0 +1,16 @@
+/*
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};*/
+class Solution
+{
+public:
+    ListNode *FindFirstCommonNode(ListNode *pHead1, ListNode *pHead2)
+    {
+        
+    }
+};
